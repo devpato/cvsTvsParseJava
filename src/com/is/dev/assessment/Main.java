7d6b507d159b84cecf67535c4b541badd284a435
mattlucian <matt@inventorysource.com>
2017-11-25 18:10:00 -0500

    added Main comments

--- src/com/is/dev/assessment/Main.java
+++ src/com/is/dev/assessment/Main.java
@@ -4,7 +4,11 @@ package com.is.dev.assessment;
 public class Main {
 
     public static void main(String[] args) {
-    
+
+        // This is the starting point for this assessment
+
+        // Read the README to learn how to begin
+
     }
 
 }
